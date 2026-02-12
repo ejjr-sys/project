a10db9ffc51dea761bd4dd4864d41a55b7f13637
ejjr-sys <savage.knuckles25@gmail.com>
2026-02-12 21:14:22 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,20 +3,27 @@ const yesBtn = document.getElementById("yesBtn");
 const modal = document.getElementById("modal");
 const openLetter = document.getElementById("openLetter");
 const closeModal = document.getElementById("closeModal");
+const mainContent = document.getElementById("mainContent");
+const success = document.getElementById("success");
+const music = document.getElementById("music");
+
 
 function moveNoButton() {
   const x = Math.random() * 200 - 100;
   const y = Math.random() * 200 - 100;
-  noBtn.style.transform = `translate(${x}px, ${y}px)`;
+  noBtn.style.transform = `translate(${x}px, ${y}px) scale(0.9)`;
 }
-
 
 noBtn.addEventListener("mouseenter", moveNoButton);
 noBtn.addEventListener("touchstart", moveNoButton);
 
+
 yesBtn.addEventListener("click", () => {
-  alert("YAYYY HEHEHE");
+  mainContent.style.display = "none";
+  success.classList.remove("hidden");
+  music.play().catch(() => {});
 });
+
 
 openLetter.addEventListener("click", () => {
   modal.style.display = "flex";
